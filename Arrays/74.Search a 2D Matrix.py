e77b545d679492f36df3bef5731c2a3937853174
Shashank S <126173294+Shashank1202@users.noreply.github.com>
2024-05-04 21:12:20 +0530

    Create 74.Search a 2D Matrix.py

---
+++ Arrays/74.Search a 2D Matrix.py
@@ -0,0 +1,23 @@
+'''
+Problem Link:|
+
+https://leetcode.com/problems/search-a-2d-matrix/description/
+'''
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n=len(matrix)
+        m=len(matrix[0])
+
+        low=0
+        high= n*m-1
+        while low<=high:
+            mid= (low+high)//2
+            row=mid//m
+            col=mid%m
+            if matrix[row][col]==target:
+                return True
+            elif matrix[row][col]<target:
+                low=mid+1
+            else:
+                high=mid-1
+        return False
