54d72f9e6fc17a9ec7b3fea0a7f8ab01f2ccbe2b
Shashank S <126173294+Shashank1202@users.noreply.github.com>
2024-05-04 19:01:51 +0530

    Create 287. Find the Duplicate Number.py

---
+++ Arrays/287. Find the Duplicate Number.py
@@ -0,0 +1,19 @@
+'''
+Problem Link:|
+
+https://leetcode.com/problems/find-the-duplicate-number/description/
+'''
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        fast=nums[0]
+        slow=nums[0]
+        while True:
+            slow= nums[slow]
+            fast= nums[nums[fast]]
+            if slow==fast:
+                break
+        fast=nums[0]
+        while slow!=fast:
+            slow=nums[slow]
+            fast=nums[fast]
+        return slow
