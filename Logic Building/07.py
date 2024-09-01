e3efd4b7aa0632cd192e445ffb62a8f07ba50811
Shashank S <shashank.s.1202@gmail.com>
2024-09-01 11:33:13 +0530

    Logic solved

--- Logic Building/07.py
+++ Logic Building/07.py
@@ -1,7 +1,11 @@
 def star(n):
     for i in range(n):
-        for j in range(i+1):
-            print(" *", end="")
+        for j in range(n-i-1):
+            print(' ', end='')
+        for j in range(2*i+1):
+            print('*', end='')
+        for j in range(n-i-1):
+            print(' ', end='')
         print()
 
 star(5)
