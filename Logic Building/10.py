ce81ebec37485e4efe8ec9a6eda2abd3ada003dc
Shashank S <shashank.s.1202@gmail.com>
2024-09-01 11:53:44 +0530

    #10 logic solved

---
+++ Logic Building/10.py
@@ -0,0 +1,12 @@
+def star(n):
+    for i in range(n):
+        for j in range(i):
+            print('*', end='')
+        print()
+    
+    for i in range(n, -1, -1):
+        for j in range(i):
+            print('*', end='')
+        print()
+
+star(5)
